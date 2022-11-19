ba8206f081e5f156135dc0768b070d2f7ac0e5a7
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2022-11-19 13:22:52 +0200

    hw 6 remove

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,13 @@
 const input = document.querySelector(`#validation-input`);
 const validLength = Number(input.dataset.length);
 
-input.addEventListener(`blur`, onInputBlur);
-
-function onInputBlur(event) {
-    event.currentTarget.value.length === validLength
-        ? input.classList.add('valid')
-        : input.classList.add('invalid');
+const onInputBlurAdd = event => {
+    if (event.currentTarget.value.length === validLength) {
+        input.classList.add("valid");
+        input.classList.remove("invalid");
+    } else {
+        input.classList.add("invalid");
+        input.classList.remove("valid");
+    }
 };
-
+input.addEventListener("blur", onInputBlurAdd);
