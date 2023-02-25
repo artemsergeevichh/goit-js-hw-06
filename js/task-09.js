b465f28dd625d2cea8c6bf764605f742aca73e69
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:17:26 +0200

    bagfix

--- js/task-09.js
+++ js/task-09.js
@@ -1,14 +1,40 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//new
+const tools = {
+  body: document.querySelector('body'),
+  button: document.querySelector('.change-color'),
+  color: document.querySelector('.color'),
 }
 
-const buttonColor = document.querySelector(`.change-color`) 
-const body = document.querySelector(`body`)
-const spanColor = document.querySelector(`.color`)
 
-buttonColor.addEventListener(`click`, el =>{
-const color = getRandomHexColor();
-body.style.backgroundColor = color;
-spanColor.textContent = color;
-
+tools.button.addEventListener('click', () => {
+  tools.body.style.backgroundColor = getRandomHexColor();
+  tools.color.textContent = name;
 });
+
+
+let name = '';
+
+function getRandomHexColor() {
+  return name =`#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+
+
+//accepted
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const buttonColor = document.querySelector(`.change-color`) 
+// const body = document.querySelector(`body`)
+// const spanColor = document.querySelector(`.color`)
+
+// buttonColor.addEventListener(`click`, el =>{
+// const color = getRandomHexColor();
+// body.style.backgroundColor = color;
+// spanColor.textContent = color;
+
+// });
