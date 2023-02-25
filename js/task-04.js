75a8c760ff53bdfcb4efa91a585cc811e8910fe6
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:11:48 +0200

    bagfix

--- js/task-04.js
+++ js/task-04.js
@@ -1,21 +1,46 @@
-const number = document.querySelector('#value');
-const decrement = document.querySelector('[data-action="decrement"]');
-const increment = document.querySelector('[data-action="increment"]');
-
-let counterValue = 0
-
-function onDecrementClick(){
-
-counterValue -=1
-number.textContent = counterValue
+//new
+const counterEl = {
+    value: document.querySelector('#value'),
+    decrement: document.querySelector('button[data-action="decrement"]'),
+    increment: document.querySelector('button[data-action="increment"]'),
 }
 
-function onIncrementClick(){
- counterValue +=1
- number.textContent = counterValue
- }
+let counterValue = 0;
 
-decrement.addEventListener(`click`, onDecrementClick)
+counterEl.decrement.addEventListener('click', () => {
+    counterValue -= 1;
+    counterEl.value.textContent = counterValue;
+});
 
-increment.addEventListener(`click`, onIncrementClick)
+counterEl.increment.addEventListener('click', () => {
+    counterValue += 1;
+    counterEl.value.textContent = counterValue;
+});
+
+counterEl.value.textContent = counterValue;
+
+
+
+//accepted
+
+// const number = document.querySelector('#value');
+// const decrement = document.querySelector('[data-action="decrement"]');
+// const increment = document.querySelector('[data-action="increment"]');
+
+// let counterValue = 0
+
+// function onDecrementClick(){
+
+// counterValue -=1
+// number.textContent = counterValue
+// }
+
+// function onIncrementClick(){
+//  counterValue +=1
+//  number.textContent = counterValue
+//  }
+
+// decrement.addEventListener(`click`, onDecrementClick)
+
+// increment.addEventListener(`click`, onIncrementClick)
 
