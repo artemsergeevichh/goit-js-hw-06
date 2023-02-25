30304b06cb05c5fa00bc89ebcfded33d33ba9af0
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:13:02 +0200

    bagfix

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,29 @@
-const input = document.querySelector('input#name-input');
-const output = document.querySelector('span#name-output');
+//new 
+const textEl = {
+    input: document.querySelector('#name-input'),
+    output: document.querySelector('#name-output'),
+};
 
-console.log(input);
-console.log(output);
-
-input.addEventListener('input', (event) => {
-    output.textContent = event.currentTarget.value;
-    if (event.currentTarget.value === ``) {
-        output.textContent = `Anonymous`;
-    }
-    console.log(output.textContent)
+textEl.input.addEventListener('input', (event) => {
+    event.currentTarget.value === ''
+    ?textEl.output.textContent = 'Anonymous'
+    :textEl.output.textContent = event.currentTarget.value;
 });
+
+
+
+
+//accepted
+// const input = document.querySelector('input#name-input');
+// const output = document.querySelector('span#name-output');
+
+// console.log(input);
+// console.log(output);
+
+// input.addEventListener('input', (event) => {
+//     output.textContent = event.currentTarget.value;
+//     if (event.currentTarget.value === ``) {
+//         output.textContent = `Anonymous`;
+//     }
+//     console.log(output.textContent)
+// });
