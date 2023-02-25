9af8c9b68edb0191216625ecb5235cfa0569cf71
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:15:33 +0200

    bagfix

--- js/task-07.js
+++ js/task-07.js
@@ -1,11 +1,25 @@
-const lengthControl = document.querySelector("#font-size-control")
-const text = document.querySelector("#text")
+//new
+const range = {
+    font: document.querySelector('#font-size-control'),
+    text: document.querySelector('#text'),
+}
 
-lengthControl.addEventListener(`input`, el => {
+range.font.addEventListener('input', event => {
+    range.text.style.fontSize = `${event.currentTarget.value}px`;
+})
 
-const fontSize = el.currentTarget.value
-console.dir(el.currentTarget.value)
 
-text.style.fontSize = `${fontSize}px`
 
-});
+
+//accepted
+// const lengthControl = document.querySelector("#font-size-control")
+// const text = document.querySelector("#text")
+
+// lengthControl.addEventListener(`input`, el => {
+
+// const fontSize = el.currentTarget.value
+// console.dir(el.currentTarget.value)
+
+// text.style.fontSize = `${fontSize}px`
+
+// });
