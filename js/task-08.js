bfb1c6fd06fdd9bcd3e642970150cbb3388ec263
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:16:16 +0200

    bagfix

--- js/task-08.js
+++ js/task-08.js
@@ -1,16 +1,36 @@
-const login = document.querySelector(`.login-form`)
-login.addEventListener(`submit`, el =>{
+//new
+const form = document.querySelector(`.login-form`);
 
-el.preventDefault()
+form.addEventListener(`submit`, el =>{
+   el.preventDefault()
 
 const data = {
- email: el.currentTarget.elements.email.value,
- password: el.currentTarget.elements.password.value
+   email: el.currentTarget.email.value,
+   password: el.currentTarget.password.value
 }
-if (data.email.trim() === `` || data.password.trim() === ``) {
- return alert(`Астанавитесь!!!`)
-} else {
- console.log(data) 
- el.currentTarget.reset()
-}
+
+data.email === '' || data.password === ''
+   ? alert(`Астанавитесь!!!`)
+   : console.log(data) 
+
+   el.currentTarget.reset()
 });
+
+
+//accepted
+// const login = document.querySelector(`.login-form`)
+// login.addEventListener(`submit`, el =>{
+
+// el.preventDefault()
+
+// const data = {
+//  email: el.currentTarget.elements.email.value,
+//  password: el.currentTarget.elements.password.value
+// }
+// if (data.email.trim() === `` || data.password.trim() === ``) {
+//  return alert(`Астанавитесь!!!`)
+// } else {
+//  console.log(data) 
+//  el.currentTarget.reset()
+// }
+// });
