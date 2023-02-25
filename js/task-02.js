05c211ea206584a186f5c495cd46fd4e40075f29
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:09:14 +0200

    bagfix

--- js/task-02.js
+++ js/task-02.js
@@ -6,13 +6,29 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+//new
+const ingredientsEl = document.querySelector('#ingredients');
 
-const list = document.querySelector("#ingredients");
-const makeElementLi = (element) => {
-  const newLi = document.createElement("li");
-  newLi.textContent = element;
-  newLi.classList.add("item");
-  return newLi;
-};
-const listItems = ingredients.map(makeElementLi);
-list.append(...listItems);
+const makeEl = ingredients.map(element => {
+  const addEl = document.createElement('li');
+  addEl.textContent = `${element}`;
+  addEl.classList.add('item');
+  return addEl;
+})
+
+ingredientsEl.prepend(...makeEl);
+
+
+
+
+//accepted
+
+// const list = document.querySelector("#ingredients");
+// const makeElementLi = (element) => {
+//   const newLi = document.createElement("li");
+//   newLi.textContent = element;
+//   newLi.classList.add("item");
+//   return newLi;
+// };
+// const listItems = ingredients.map(makeElementLi);
+// list.append(...listItems);
