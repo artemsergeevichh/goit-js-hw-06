e4b5c8afdc29af8da532110fefd48d9393a44c04
artemsergeevichh <107759597+artemsergeevichh@users.noreply.github.com>
2023-02-25 19:05:29 +0200

    bagfix

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,20 @@
-const categoriesItems = document.querySelectorAll('#categories > .item'); 
-console.log('Number of categories: ', categoriesItems.length); 
-categoriesItems.forEach(element => { 
-    console.log('Category: ', element.querySelector('h2').textContent); 
-    console.log('Elements: ', element.querySelectorAll('li').length); 
-});
+//new
+const itemEl = document.querySelectorAll('#categories .item');
+
+console.log(`Number of categories: ${itemEl.length}`);
+
+itemEl.forEach(element => {
+    console.log(`Category: ${element.querySelector('h2').textContent}`);
+    console.log(`Elements: ${element.querySelectorAll('li').length}`);
+})
+
+
+//accepted
+
+
+// const categoriesItems = document.querySelectorAll('#categories > .item'); 
+// console.log('Number of categories: ', categoriesItems.length); 
+// categoriesItems.forEach(element => { 
+//     console.log('Category: ', element.querySelector('h2').textContent); 
+//     console.log('Elements: ', element.querySelectorAll('li').length); 
+// });
